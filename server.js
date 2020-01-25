691ff7b90ebb349d27fd93322f5d4992eeff944e
Rory Letteney <rletteney@msuite.tech>
2020-01-25 13:42:49 -0600

    Added eslint and swagger documentation.

--- server.js
+++ server.js
@@ -4,11 +4,27 @@
 require("dotenv").config();
 const express = require("express");
 const database = require("./database");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
 
 // SETUP
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3300;
+const swaggerDefinition = {
+  info: {
+    title: "Letteney Software API",
+    version: "1.0.0",
+    description: "API docs for letteney.software"
+  },
+  basePath: "/api/"
+};
+const swaggerOptions = {
+  swaggerDefinition,
+  apis: ["./routes/*.js"]
+};
+const swaggerDocument = swaggerJSDoc(swaggerOptions);
 app.use(express.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // ROUTES
 app.get("/", async (req, res) => {
