9f19b4508cd3a285a0eedf7ed433539cb259b4db
Rory Letteney <rletteney@msuite.tech>
2020-01-27 14:00:52 -0600

    DELETE tags failing tests written

--- test/tagsRoutes.test.js
+++ test/tagsRoutes.test.js
@@ -148,4 +148,70 @@ describe("tags-routes", () => {
         });
     });
   });
+
+  describe("DELETE /api/tags/:id", () => {
+    let createdTagId;
+
+    before(() => {
+      return request
+        .post("/api/tags")
+        .send({ name: "test-tag-delete" })
+        .then(res => {
+          return createdTagId = JSON.parse(res.text)[0].id;
+        });
+    });
+
+    it("should return 200 and return the list of tags without the deleted tag", () => {
+      return request
+        .delete(`/api/tags/${createdTagId}`)
+        .expect(200)
+        .expect(res => {
+          expect(res.text).to.be.a("string");
+
+          const response = JSON.parse(res.text);
+
+          expect(response).to.be.an("array");
+          expect(response[0]).to.be.an("object");
+          expect(response[0]).to.have.own.property("id");
+          expect(response[0]).to.have.own.property("name");
+          expect(response.findIndex(r => r.id === createdTagId)).to.equal(-1);
+        });
+    });
+
+    it("should return status 400 when no id sent", () => {
+      return request
+        .delete(`/api/tags/null`)
+        .expect(400)
+        .expect(res => {
+          expect(res.text).to.be.a("string");
+
+          const response = JSON.parse(res.text);
+
+          expect(response).to.be.an("object");
+          expect(response).to.have.own.property("error");
+          expect(response.error)
+            .to.have.own.property("status")
+            .and.to.equal(400);
+          expect(response.error).to.have.own.property("message");
+        });
+    });
+
+    it("should return status 500 when the tag to delete is not found", () => {
+      return request
+        .delete("/api/tags/0")
+        .expect(500)
+        .expect(res => {
+          expect(res.text).to.be.a("string");
+
+          const response = JSON.parse(res.text);
+
+          expect(response).to.be.an("object");
+          expect(response).to.have.own.property("error");
+          expect(response.error)
+            .to.have.own.property("status")
+            .and.to.equal(500);
+          expect(response.error).to.have.own.property("message");
+        });
+    });
+  });
 });
